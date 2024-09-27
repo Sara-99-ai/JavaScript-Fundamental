894381c1153911cc19fc672205b49de931db95d5
Sara Ahmed <ssarrraa185@gmail.com>
2024-09-27 17:30:05 +0300

    Fundamental 1

---
+++ index.js
@@ -0,0 +1,67 @@
+/*let firstName = "sara"
+ console.log(firstName);
+
+ firstName= 25;
+ console.log(firstName);
+
+ firstName=true;
+ console.log(firstName);*/
+
+/*let x = 26
+let y = 5
+
+console.log(x + y);
+console.log(x - y);
+console.log(x % y);
+console.log(x / y);
+console.log(x ** y);
+console.log(x * y);*/
+
+// Data 1 :-
+let Markheight=1.69;
+let Markweight=78;
+let Johnheight=1.95;
+let Johnweight=92;
+
+
+let BMIMark=Markweight/(Markheight**2);
+let BMIJohn=Johnweight/(Johnheight**2);
+console.log(BMIMark, BMIJohn);
+
+//Data 2:-
+Markheight=1.88;
+Markweight=95;
+Johnheight=1.76;
+Johnweight=85;
+BMIMark=Markweight/(Markheight**2);
+console.log(BMIJohn,BMIMark);
+
+BMIJohn=Johnweight/(Johnheight**2);
+
+/*if (BMIMark>BMIJohn) {
+    console.log('Mark is bigger than john');
+} 
+else if (BMIJohn>BMIMark){
+    console.log('john is bigger than mark');
+}*/
+
+let age = 22 
+age >= 18 ? console.log('yes you can drive') : console.log('no you must wait');
+
+/*BMI=BMIMark>BMIJohn;
+console.log(BMI);*/
+
+
+/*//Data 2:-
+Markheight=1.88;
+Markweight=95;
+Johnheight=1.76;
+Johnweight=85;
+BMIMark=Markweight/(Markheight**2);
+console.log(BMIMark,BMIJohn);
+
+BMIJohn=Johnweight/(Johnheight**2); */
+
+
+/*BMI=BMIMark>BMIJohn;
+console.log(BMI); */
